0f49880cbe64c2dd28bdc574b175660f259e5dc5
Valverde Daniel <valverdedaniel44@gmail.com>
2018-12-18 20:36:27 -0500

    portfolio update 2

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "example@example.net";
+$address = "valverdedaniel44@gmail.com";
 
 
 // Configuration option.
